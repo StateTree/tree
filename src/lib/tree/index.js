382f97b5b322baa455a15708be964d386edfb6ce
Sanjay krishna anbalagan <sanjay.krishna@cengage.com>
2017-12-22 12:45:25 -0500

    Release Commit

--- src/lib/tree/index.js
+++ src/lib/tree/index.js
@@ -4,33 +4,36 @@ import Node from './Node';
 export default class Tree {
 	constructor(cache = true ) {
 		this._keymap = cache ? {} : null;
-		this.root = null;
+		this.root;
+		this.rootKey;
 	}
 }
 
-Tree.prototype.insert = function ( childValue, parentKey, childKey,callback){
+Tree.prototype.insert = function ( childValue, parentKey = this.rootKey, childKey,callback){
 	if( (childKey !== undefined) && typeof childKey !== 'string'){
-		console.warn('childKey can be only string if provioded');
+		console.warn('childKey can be only string if provided');
+		return;
+	}
+
+	if( (parentKey !== undefined) && typeof parentKey !== 'string'){
+		console.warn('parentKey can be only string if provided');
 		return;
 	}
 
 	if(this._keymap){
 		let parentNode = null;
 		if(parentKey !== undefined){
-			if( typeof parentKey !== 'string'){
-				console.warn('parentKey can be only string if provided');
-				return;
-			}
 			parentNode = this._keymap[parentKey];
-			if(!parentNode && this.root){
+			if(!parentNode){
 				console.warn('parent not found: ', parentKey);
 				return;
 			}
 		}
 
 		const newChild = new Node(childValue, childKey);
-		if(!this.root){
+		if(!this.root){ // first insert will become the rootNode
 			this.root = newChild;
+			this.rootKey = newChild.key;
 		} else {
 			parentNode.add(newChild, parentNode.key);
 		}
